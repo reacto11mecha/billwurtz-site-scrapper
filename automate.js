78ab2ff5855df0e91741d5638a32f7d0b324759e
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2021-01-25 18:54:18 +0700

    Refactor code into Listr task

--- automate.js
+++ automate.js
@@ -1,30 +1,81 @@
 const config = require("./config.json");
 const { videos, songs, notebook } = require("./scrapper");
 const { jsonFormatter } = require("./utils");
+const Listr = require("listr");
 const path = require("path");
 const fs = require("fs");
 
 const outputDir = path.join(__dirname, config.outputDirectory);
-if (!fs.existsSync(outputDir)) fs.mkdirSync(outputDir);
 
-(async function () {
-  const songList = await songs();
-  const videoList = await videos();
-  const noteList = await notebook();
+const task = new Listr([
+  {
+    title: "Check Output Directory",
+    task: async () =>
+      await Promise.all([
+        !fs.existsSync(outputDir) && fs.mkdirSync(outputDir),
+      ]).then(() => Promise.resolve()),
+  },
+  {
+    title: "Scrapping Songs",
+    task: async (ctx) => {
+      try {
+        const songList = await songs();
+        const songsJson = jsonFormatter(songList);
 
-  const all = jsonFormatter({
-    song: songList,
-    video: videoList,
-    notebook: noteList,
-  });
-  const songsJson = jsonFormatter(songList);
-  const videoJson = jsonFormatter(videoList);
-  const noteJson = jsonFormatter(noteList);
+        ctx.song = songList;
 
-  Promise.all([
-    fs.writeFileSync(path.join(outputDir, "all.json"), all),
-    fs.writeFileSync(path.join(outputDir, "songs.json"), songsJson),
-    fs.writeFileSync(path.join(outputDir, "videos.json"), videoJson),
-    fs.writeFileSync(path.join(outputDir, "notebook.json"), noteJson),
-  ]).then(() => console.log("File writed"));
-})();
+        return Promise.all([
+          fs.writeFileSync(path.join(outputDir, "songs.json"), songsJson),
+        ]).then(() => Promise.resolve("Songs File Writed"));
+      } catch (e) {
+        return Promise.reject(e);
+      }
+    },
+  },
+  {
+    title: "Scrapping Video",
+    task: async (ctx) => {
+      try {
+        const videoList = await videos();
+        const videoJson = jsonFormatter(videoList);
+
+        ctx.video = videoList;
+
+        return Promise.all([
+          fs.writeFileSync(path.join(outputDir, "videos.json"), videoJson),
+        ]).then(() => Promise.resolve("Video File Writed"));
+      } catch (e) {
+        return Promise.reject(e);
+      }
+    },
+  },
+  {
+    title: "Scrapping Notebook",
+    task: async (ctx) => {
+      try {
+        const noteList = await notebook();
+        const noteJson = jsonFormatter(noteList);
+
+        ctx.note = noteList;
+
+        return Promise.all([
+          fs.writeFileSync(path.join(outputDir, "notebook.json"), noteJson),
+        ]).then(() => Promise.resolve("Video File Writed"));
+      } catch (e) {
+        return Promise.reject(e);
+      }
+    },
+  },
+  {
+    title: "Combine All Data",
+    task: async (ctx) => {
+      const allJson = jsonFormatter({ ...ctx });
+
+      return Promise.all([
+        fs.writeFileSync(path.join(outputDir, "all.json"), allJson),
+      ]).then(() => Promise.resolve("Combined File Writed"));
+    },
+  },
+]);
+
+task.run();
